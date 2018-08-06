f60ce94f31387e0ce12ee8d9897881473faf147c
Sk961023 <1103379079@qq.com>
2018-08-06 22:43:44 +0800

    123

---
+++ Vue-route1/vuecliTest/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <router-link to="/">首页</router-link> |
+      <router-link to="/Hi">Hi</router-link> |
+      <router-link :to="{name:'Hi1',params:{username:'Hello',id:'888'}}">Hi1111</router-link> |
+      <router-link to="/Hi/Hi2">Hi2222</router-link>
+    </div>
+    <p>{{$route.name}}</p>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
