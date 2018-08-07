b43ccc8081674df188866ea39183079f74539555
Sk961023 <1103379079@qq.com>
2018-08-07 18:43:19 +0800

    213

---
+++ Vue-route2/my-project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <router-view name="left" style="float:left;width:50%;height:300px;background:#ccc"/>
+    <router-view name="right" style="float:right;width:50%;height:300px;background:red"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
